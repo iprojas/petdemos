c65aa64fc22f836c42ab1baf0adbd5c441be4048
iprojas <rojas.ip@gmail.com>
2024-01-10 03:28:24 -0600

    Update script.js

--- interactive-particles-text-create-with-three-js/script.js
+++ interactive-particles-text-create-with-three-js/script.js
@@ -97,7 +97,7 @@ class CreateParticles {
 
 		this.data = {
 
-			text: 'FUTURE\nIS NOW',
+			text: 'INTERACTIVE\nIMAGINATION',
 			amount: 1500,
 			particleSize: 1,
 			particleColor: 0xffffff,
@@ -115,7 +115,7 @@ class CreateParticles {
 	setup(){
 
 		const geometry = new THREE.PlaneGeometry( this.visibleWidthAtZDepth( 100, this.camera ), this.visibleHeightAtZDepth( 100, this.camera ));
-		const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, transparent: true } );
+		const material = new THREE.MeshBasicMaterial( { color: 0xff0000, transparent: true } );
 		this.planeArea = new THREE.Mesh( geometry, material );
 		this.planeArea.visible = false;
 		this.createText();
